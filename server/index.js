d838111b13c5a7439d3389348f4a34bfd681264d
Tayyab Tariq <tayyabashraf22@gmail.com>
2023-11-29 05:06:40 +0500

    Proxy Update

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(cors({
   credentials: true,
   // origin: 'https://devtt-real-estate.netlify.app/',
-  origin: 'http://localhost:5173/',
+  origin: 'http://localhost:5173',
 }));
 
 app.listen(process.env.PORT || 3000, () => {
