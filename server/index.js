353de85e38a26ac228779c97d835801c2b1fa4d8
Tayyab Tariq <tayyabashraf22@gmail.com>
2023-12-04 16:59:02 +0500

    Client Connection

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'https://devtt-real-estate.netlify.app',
+  origin: process.env.CLIENT_URL,
 }));
 
 app.listen(process.env.PORT || 3000, () => {
